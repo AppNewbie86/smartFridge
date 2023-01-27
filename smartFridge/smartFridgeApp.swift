46f47c2461c0658ab9d82aa05c11e16b7de4f1fe
Till Hemmerich <till.hemmerich@syntax-institut.de>
2023-01-27 09:14:03 +0100

    Initial Commit

---
+++ smartFridge/smartFridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  smartFridgeApp.swift
+//  smartFridge
+//
+//  Created by Till Hemmerich on 27.01.23.
+//
+
+import SwiftUI
+
+@main
+struct smartFridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
